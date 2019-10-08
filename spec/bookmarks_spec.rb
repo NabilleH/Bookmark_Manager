f9337fa257128fde145c750be53525265c0c5183
DanielleInkster <danielle.inkster@gmail.com>
2019-10-08 14:32:31 +0100

    Setup test database

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -4,8 +4,15 @@ describe Bookmark do
 
   describe '#all' do
     it "should return all bookmarks stored" do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
       bookmarks = Bookmark.all
-      expect(bookmarks).to include("http://www.stackoverflow.com")
+      expect(bookmarks).to include("http://www.makersacademy.com")
+      expect(bookmarks).to include("http://www.destroyallsoftware.com")
       expect(bookmarks).to include("http://www.google.com")
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,16 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require_relative './setup_test_database'
+
+ENV['ENVIRONMENT']='test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
+
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara/rspec'
@@ -20,8 +30,6 @@ require 'capybara'
 require 'rspec'
 
 Capybara.app = BookmarkManager
-
-ENV['RACK_ENV']='test'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
