cd4e1d9da7d77acd129222710e395b1feeca8b49
Nabille Helal <nabilleh@outlook.com>
2019-10-08 16:37:42 +0100

    Update bookmark class

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -23,12 +23,15 @@ class Bookmark
   end
 
   def self.create(url:, title:)
+    return false unless is_url?(url)
     if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
+     connection = PG.connect(dbname: 'bookmark_manager_test')
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
 
-    connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
+    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
+
 end
