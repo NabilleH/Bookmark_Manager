95872c3be62d4437578a0c9055a97d6c1eebbe29
Nabille Helal <nabilleh@outlook.com>
2019-10-08 16:26:02 +0100

    Add Testing ID persist

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -10,9 +10,9 @@ describe Bookmark do
     bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
     Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
     Bookmark.create(url: "http://www.google.com", title: "Google")
- 
+
     bookmarks = Bookmark.all
- 
+
     expect(bookmarks.length).to eq 3
     expect(bookmarks.first).to be_a Bookmark
     expect(bookmarks.first.id).to eq bookmark.id
@@ -21,12 +21,15 @@ describe Bookmark do
    end
  end
 
-  describe "#create" do
+ describe '.create' do
   it 'creates a new bookmark' do
-    bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark').first
+    bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+    persisted_data = persisted_data(id: bookmark.id)
 
-    expect(bookmark['url']).to eq 'http://www.testbookmark.com'
-    expect(bookmark['title']).to eq 'Test Bookmark'
+    expect(bookmark).to be_a Bookmark
+    expect(bookmark.id).to eq persisted_data.first['id']
+    expect(bookmark.title).to eq 'Test Bookmark'
+    expect(bookmark.url).to eq 'http://www.testbookmark.com'
   end
 end
 
