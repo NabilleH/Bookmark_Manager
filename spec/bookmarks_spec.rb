8d69419b25855d40fe90cd5435d3067bac9b0ad5
DanielleInkster <danielle.inkster@gmail.com>
2019-10-08 16:00:52 +0100

    Adding titles unfinished

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -1,28 +1,34 @@
 require 'bookmark'
+require 'database_helpers'
 
 describe Bookmark do
 
   describe '#all' do
-    it "should return all bookmarks stored" do
-      connection = PG.connect(dbname: 'bookmark_manager_test')
+  it 'returns a list of bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
-
-      bookmarks = Bookmark.all
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
-    end
-  end
+    bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+    Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+    Bookmark.create(url: "http://www.google.com", title: "Google")
+ 
+    bookmarks = Bookmark.all
+ 
+    expect(bookmarks.length).to eq 3
+    expect(bookmarks.first).to be_a Bookmark
+    expect(bookmarks.first.id).to eq bookmark.id
+    expect(bookmarks.first.title).to eq 'Makers Academy'
+    expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
+   end
+ end
 
   describe "#create" do
-    it 'creates a new bookmark' do
-      Bookmark.create(url: 'http://www.testbookmark.com')
-      expect(Bookmark.all).to include 'http://www.testbookmark.com'
-    end
+  it 'creates a new bookmark' do
+    bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark').first
+
+    expect(bookmark['url']).to eq 'http://www.testbookmark.com'
+    expect(bookmark['title']).to eq 'Test Bookmark'
   end
+end
 
 end
 
--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -1,8 +1,11 @@
 feature "creating a bookmark" do
-  scenario "A user can add a bookmark to Bookmark Manager" do
-    visit '/bookmarks/new'
-    fill_in('url', with: 'http://www.microsoft.com')
+
+  scenario 'A user can add a bookmark to Bookmark Manager' do
+    visit('/bookmarks/new')
+    fill_in('url', with: 'http://www.testbookmark.com')
+    fill_in('title', with: 'Test Bookmark')
     click_button('Submit')
-    expect(page).to have_content "http://www.microsoft.com"
+
+    expect(page).to have_link('Test Bookmark', href: 'http://www.testbookmark.com')
   end
 end
