9b43a0f0131e1c48b0e7b86d103ba2856a61d2cb
Nabille Helal <nabilleh@outlook.com>
2019-10-09 14:06:09 +0100

    Add Update bookmark

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions, :method_override
+
   get '/' do
 
   end
@@ -21,10 +23,18 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
-  enable :sessions, :method_override
-
   delete '/bookmarks/:id' do
     Bookmark.delete(id: params[:id])
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    @bookmark_id = params[:id]
+    erb :'bookmarks/edit'
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
     redirect '/bookmarks'
   end
 
