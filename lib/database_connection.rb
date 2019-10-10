6e4a0aed3178ea8e82a2baadac6cd1f7e5a794ad
Nabille Helal <nabilleh@outlook.com>
2019-10-10 11:36:27 +0100

    Update Database Connection

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 class DatabaseConnection
-  def self.setup(dbname)
+  def self.setup(dbname:)
     @connection = PG.connect(dbname: dbname)
   end
 
@@ -12,5 +12,5 @@ class DatabaseConnection
   def self.query(sql)
     @connection.exec(sql)
   end
-  
+
 end
